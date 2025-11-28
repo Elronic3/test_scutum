edfd62198a14b7d7f3e5a913ce353830f66f28cc
Elronic3 <kuvarzin3229@gmail.com>
2025-11-28 11:15:59 +0200

    structure + libs

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  // Map Weather
+  static const String weatherBaseUrl = '';
+  static const String weatherApiKey = '';
+  static const String defaultCity = 'Kyiv';
+
+  // Storage
+  static const String taskStorageKey = 'tasks_local_data';
+
+  // UI
+  static const double padding = 20.0;
+  static const double borderRadius = 16.0;
+}
